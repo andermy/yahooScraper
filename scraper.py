81f64cb9452d5915e5a93e97340295c4f868f4c3
Myhr <anders.myhr@siemens.com>
2020-06-08 22:57:40 +0200

    Test

--- scraper.py
+++ scraper.py
@@ -66,7 +66,7 @@ def get_datestamp():
     dd = date.day
 
     # must use 12:30 for Unix time instead of 4:30 NY time 
-    next_close = datetime.datetime(yy, mm, dd, 12, 30)
+    next_close = datetime.datetime(yy, mm, dd, 0, 0)
 
     return next_close
 
