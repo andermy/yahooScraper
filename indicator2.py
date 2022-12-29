b0c9e652b2468accd0d0abc0eaf2d58c684d1d37
Anders Myhr <andermy@gmail.com>
2022-12-29 07:29:51 +0100

    dfd

--- indicator2.py
+++ indicator2.py
@@ -369,7 +369,7 @@ def main():
     start = int(6 * (day_of_month-1)) + 100*odd
     if start-9 <= len(tickers):
         print("running data collection")
-        for tick in tickers[start+4:start+6]:
+        for tick in tickers[start+4:start+5]:
             try:
                 o = Options(tick)
                 o.regression()
